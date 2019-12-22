bc361b7ea03c2447a7780953bffa1c8d4a998fd7
Alexander <Snakeage@gmail.com>
2019-12-22 17:47:25 +0300

    yoga modal2

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -88,7 +88,7 @@ window.addEventListener('DOMContentLoaded', function() {
     //Modal
 
     let more = document.querySelector('.more'),
-        descriptionBtn = document.querySelector('.description-btn'),
+        descriptionBtn = document.querySelectorAll('.description-btn'),
         overlay = document.querySelector('.overlay'),
         close = document.querySelector('.popup-close');
 
@@ -98,11 +98,13 @@ window.addEventListener('DOMContentLoaded', function() {
         document.body.style.overflow = 'hidden';
     });
 
-    descriptionBtn.addEventListener('click', function() {
-        overlay.style.display = 'block';
-        this.classList.add('more-splash');
-        document.body.style.overflow = 'hidden';
-    });
+    for (let i = 0; i < descriptionBtn.length; i++) {
+        descriptionBtn[i].addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        })
+    };
 
     close.addEventListener('click', function() {
         overlay.style.display = 'none';
